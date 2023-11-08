63c0c58e6b64b210cbdf028d0aac4c0effb85b93
Olugbade Ayodeji <olugbade.ayodeji@gmail.com>
2023-11-08 16:51:04 +0100

    installed and use axios

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-index.html
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+
+    axios.get('https://apiv2.bitcoinaverage.com/indices/global/ticker/all?crypto=BTC&fiat=USD,EUR')
+    .then(response => {
+        console.log(res);
+        //console.log
+        res.send(res.data.status)
+    })
+    .catch(error => {
+        console.log(error);
+    });
+    // res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body.crypto);
+})
+
+app.listen(3000, function() {
+    console.log('listening on port 3000')
+});
+
+ 
